56405021afc7ac0304118e6e5670f57669dbb504
Stefan Spita <stefan.spita@moneyhub.com>
2018-09-03 16:32:52 +0100

    Add get-authorize-url script
    MH3-1087

---
+++ examples/get-authorize-url.js
@@ -0,0 +1,36 @@
+const Moneyhub = require("../src/index")
+const config = require("./config")
+
+const DEFAULT_BANK_ID = "4ddeccd5a66881eb25223d5ff8b2e2c1"
+const DEFAULT_STATE = "foo"
+
+console.log("\n\nUsage: `node get-authorize-url.js bankId[optional] state[optional]` \n\n")
+
+const [
+  bankId = DEFAULT_BANK_ID, state = DEFAULT_STATE,
+] = process.argv.slice(2)
+
+if (!bankId) throw new Error("bank id needs to be provided")
+
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+    const data = await moneyhub.getAuthorizeUrl({
+      state,
+      scope: `openid offline_access id:${bankId}`,
+      claims: {
+        "id_token": {
+          "mh:income_level": {essential: true},
+          "mh:spending_level": {essential: true},
+        },
+      }
+    })
+    console.log(data)
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
