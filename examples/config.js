78b64ed2a076b039994420ff20b98f7eb7be43be
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2018-11-07 12:13:23 +0000

    Update example config

--- examples/config.js
+++ examples/config.js
@@ -1,7 +1,7 @@
 
 const config = {
-  resourceServerUrl: "https://api-test.moneyhub.co.uk/v2.0",
-  identityServiceUrl: "https://identity-test.moneyhub.co.uk/oidc",
+  resourceServerUrl: "https://api.moneyhub.co.uk/v2.0",
+  identityServiceUrl: "https://identity.moneyhub.co.uk/oidc",
   client: {
     client_id: "your client id",
     client_secret: "your client secret",
