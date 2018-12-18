452dc1ee2e5e438792ffc760fcdd72db44fb5b69
Dave Tonge <dave.tonge@momentumft.co.uk>
2018-12-18 15:54:06 +0100

    Improve the instructions output from the make-keys file

--- examples/payments/make-keys.js
+++ examples/payments/make-keys.js
@@ -7,7 +7,13 @@ const start = async () => {
   await keystore.generate("RSA", 2048, {use: "sig"})
   const public = keystore.toJSON()
   const private = keystore.toJSON(true)
-  console.log(JSON.stringify({public, private}, null, 4))
+  public.keys[0].alg = "RS256"
+  console.log("Paste this in the jwks sectio in the admin portal:")
+  console.log(JSON.stringify(public, null, 4))
+  console.log("---------------")
+  console.log("Paste this into the keys section of the config.js file in this repo")
+  console.log(JSON.stringify(private.keys, null, 4))
+
 }
 
 start()
