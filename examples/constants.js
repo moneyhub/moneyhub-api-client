fbe9d8c6f86f44d53643ae9dedb2418a9c8b197a
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-03-14 15:10:41 +0000

    Add connection ids to constants

--- examples/constants.js
+++ examples/constants.js
@@ -4,8 +4,12 @@ const DEFAULT_DATA_SCOPES_USE_CASE_2 =
 "accounts:read accounts:write:all transactions:read:all transactions:write:all categories:read categories:write spending_goals:read spending_goals:write:all savings_goals:read savings_goals:write:all"
 
 const BANK_IDS = {
+  ALL: "all",
+  LEGACY: "legacy",
+  TEST: "test",
   MONZO: "fa37a6ecc38eea38bdf3dd0fdcb68fab",
   DAG_BANK_TEST: "1b3cd579899b5f5b666c15561a48c8b6",
+  DAG_INVESTMENT_TEST: "06995ed5c6c6f25e323b34dc45968426",
   MODELO_OPEN_BANKING_TEST: "b74f1a79f0be8bdb857d82d0f041d7d2",
 }
 const DEFAULT_BANK_ID = BANK_IDS.MODELO_OPEN_BANKING_TEST
