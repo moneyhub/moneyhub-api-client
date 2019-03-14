ea75177d9f996fbc7e642de5547b771e386b74f6
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-03-14 15:12:01 +0000

    Change widget example to take urls from config

--- examples/config.js
+++ examples/config.js
@@ -2,6 +2,7 @@
 const config = {
   resourceServerUrl: "https://api.moneyhub.co.uk/v2.0",
   identityServiceUrl: "https://identity.moneyhub.co.uk/oidc",
+  accountConnectUrl: "https://bank-chooser.moneyhub.co.uk/account-connect.js",
   client: {
     client_id: "your client id",
     client_secret: "your client secret",
--- examples/widget/index.js
+++ examples/widget/index.js
@@ -3,26 +3,50 @@ const bodyParser = require("body-parser")
 const Moneyhub = require("../../src")
 
 const config = require("../config")
+const LOCAL_IDENTITY_URL = "http://identity.dev.127.0.0.1.nip.io/oidc"
+const LOCAL_ACCOUNT_CONNECT_URL = "http://localhost:8080/account-connect.js" // Bank chooser
+const LOCAL_REDIRECT_URI = "http://localhost:3001"
+
+// Make sure to set 'http://localhost:3001' as redirect_uri for your API client
+// if you want the code exchange to be managed automatically
+
+// Run node examples/widget/index.js
+
+// Go to
+//       http://localhost:3001?userId=your-user-id
+
+const DEFAULT_USER_ID = "5c82710d7c2eb82b175c2c5c"
 
 const run = async () => {
+  const {
+    identityServiceUrl = LOCAL_IDENTITY_URL,
+    accountConnectUrl = LOCAL_ACCOUNT_CONNECT_URL,
+    client: {
+      client_id: clientId,
+      redirect_uri = LOCAL_REDIRECT_URI,
+    },
+  } = config
   const moneyhub = await Moneyhub(config)
   const app = express()
+  const [identityUrl] = identityServiceUrl.split("/oidc")
 
   app.get("/", (req, res) => {
+    const {userId = DEFAULT_USER_ID} = req.query || {}
+
     res.send(`
     <html>
     <body>
     <h3>Example page</h3>
     <p>Widget should appear below here:</p>
-    <script 
-    data-clientid="48a5db5a-b80a-4aa4-9201-41d86add6f01" 
-    data-redirecturi="http://localhost:3001" 
-    data-userid="5c82710d7c2eb82b175c2c5c" 
-    data-posturi="/result" 
+    <script
+    data-clientid="${clientId}"
+    data-redirecturi="${redirect_uri}"
+    data-userid="${userId}"
+    data-posturi="/result"
     data-finishuri="/finish"
     data-type="test"
-    data-identityuri="http://identity.dev.127.0.0.1.nip.io" 
-    src="http://localhost:8080/account-connect.js"></script>
+    data-identityuri="${identityUrl}"
+    src="${accountConnectUrl}"></script>
 
     `)
   })
@@ -30,7 +54,7 @@ const run = async () => {
   app.get("/finish", (req, res) => {
     res.send(`
       <h3>Finish</h3>
-      <a href="/">Start again</a>    
+      <a href="/">Start again</a>
     `)
   })
 
