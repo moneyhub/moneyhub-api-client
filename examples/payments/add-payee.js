e865b8d831795385bea05906ed5e41906e97bcca
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-05-07 12:20:15 -0500

    Add example script for get and add payee

---
+++ examples/payments/add-payee.js
@@ -0,0 +1,40 @@
+const commandLineArgs = require("command-line-args")
+const commandLineUsage = require("command-line-usage")
+const Moneyhub = require("../../src/index")
+const config = require("../config")
+
+const optionDefinitions = [
+  {name: "account-number", alias: "a", type: String, description: "required"},
+  {name: "sort-code", alias: "s", type: String, description: "required"},
+  {name: "name", alias: "n", type: String, description: "required"},
+]
+
+const usage = commandLineUsage(
+  {
+    header: "Options",
+    optionList: optionDefinitions,
+  }
+)
+const options = commandLineArgs(optionDefinitions)
+
+console.log(usage)
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+
+    const result = await moneyhub.addPayee({
+      accountNumber: options["account-number"],
+      sortCode: options["sort-code"],
+      name: options.name,
+    })
+
+    console.log(result)
+
+  } catch (e) {
+    console.log(e)
+    console.log(e.response.body)
+  }
+}
+
+start()
