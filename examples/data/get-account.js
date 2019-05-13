369e6f1016a1032350d0905b4335a3c0678864e0
Lewis Morris <lewis@lewismorris.co.uk>
2019-05-13 14:29:55 +0100

    Add example & update readme with code snippet for getAccount method. Update getAccount method to take a userId

---
+++ examples/data/get-account.js
@@ -0,0 +1,23 @@
+const commandLineArgs = require("command-line-args")
+const Moneyhub = require("../../src/index")
+const config = require("../config")
+
+const optionDefinitions = [
+  {name: "accountId", alias: "a", type: String},
+  {name: "userId", alias: "u", type: String},
+]
+
+const options = commandLineArgs(optionDefinitions)
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+
+    const result = await moneyhub.getAccount(options.accountId, options.userId)
+    console.log(JSON.stringify(result, null, 2))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
