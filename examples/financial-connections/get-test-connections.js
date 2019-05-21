90125c84046c1c97977d2ec0cd00970b193e8e85
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-05-21 09:14:11 +0100

    Add exmaples for financial connections

---
+++ examples/financial-connections/get-test-connections.js
@@ -0,0 +1,15 @@
+const Moneyhub = require("../../src/index")
+const config = require("../config")
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+
+    const result = await moneyhub.listTestConnections()
+    console.log(JSON.stringify(result, null, 2))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
