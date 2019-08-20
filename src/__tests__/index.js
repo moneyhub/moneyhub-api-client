d54000127172662f81231b73b5b63a6b471b5178
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2019-08-20 10:25:17 +0100

    Add basic test for client configuration

---
+++ src/__tests__/index.js
@@ -0,0 +1,12 @@
+const Moneyhub = require("../")
+const config = require("../../examples/config")
+const {expect} = require("chai")
+
+describe("API client", function() {
+  describe("Client configuration", () => {
+    it("should create client", async () => {
+      const moneyhub = await Moneyhub(config)
+      expect(moneyhub).to.be.an("object")
+    })
+  })
+})
