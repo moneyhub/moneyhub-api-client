fbd7dffa562f9b03013823cc071a0d708e2cf7a3
Gilbert Holland-Lloyd <gilbert.holland-lloyd@moneyhub.com>
2020-07-10 13:37:38 +0100

    Update get transactions
    MH3-4204

--- examples/data/get-transactions.js
+++ examples/data/get-transactions.js
@@ -8,6 +8,7 @@ const optionDefinitions = [
   {name: "userId", alias: "u", type: String, description: "required"},
   {name: "accountId", alias: "a", type: String},
   {name: "categoryId", alias: "c", type: String},
+  {name: "projectId", alias: "p", type: String},
   {name: "startDate", alias: "s", type: String},
   {name: "endDate", alias: "e", type: String},
   {name: "limit", alias: "l", type: Number},
