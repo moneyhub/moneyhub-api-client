163adbd2cea6aa33328e209ba6d622d035e4014d
Gilbert Holland-Lloyd <gilbert.holland-lloyd@moneyhub.com>
2020-08-25 11:59:23 +0100

    Add tax return to moneyhub client
    MH3-4206

---
+++ examples/data/get-tax-return.js
@@ -0,0 +1,40 @@
+const commandLineArgs = require("command-line-args")
+const commandLineUsage = require("command-line-usage")
+const Moneyhub = require("../../src/index")
+const config = require("../config")
+
+const optionDefinitions = [
+  {name: "userId", alias: "u", type: String, description: "required"},
+  {name: "startDate", alias: "s", type: String, description: "required"},
+  {name: "endDate", alias: "e", type: String, description: "required"},
+  {name: "accountId", alias: "a", type: String},
+  {name: "projectId", alias: "p", type: String},
+]
+
+const usage = commandLineUsage(
+  {
+    header: "Options",
+    optionList: optionDefinitions,
+  }
+)
+console.log(usage)
+
+const options = commandLineArgs(optionDefinitions)
+
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+
+    const result = await moneyhub.getTaxReturn(options.userId, options.startDate, options.endDate, {
+      accountId: options.accountId,
+      projectId: options.projectId,
+    })
+    console.log(JSON.stringify(result, null, 2))
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
