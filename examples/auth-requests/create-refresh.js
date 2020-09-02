e5c7b391fa8e25fdd992d097e8714a7fe17012b4
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2020-09-02 13:24:51 +0100

    Add exmaples scripts for reauth and refresh

---
+++ examples/auth-requests/create-refresh.js
@@ -0,0 +1,40 @@
+const Moneyhub = require("../../src/index")
+const config = require("../config")
+
+const commandLineArgs = require("command-line-args")
+const commandLineUsage = require("command-line-usage")
+
+const optionDefinitions = [
+  {name: "userId", alias: "u", type: String, description: "required"},
+  {name: "connectionId", alias: "c", type: String, description: "required"},
+]
+
+const usage = commandLineUsage({
+  header: "Options",
+  optionList: optionDefinitions,
+})
+console.log(usage)
+
+const options = commandLineArgs(optionDefinitions)
+
+const {userId, connectionId} = options
+
+if (!userId) throw new Error("userId is required")
+if (!connectionId) throw new Error("connectionId is required")
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+    const data = await moneyhub.createAuthRequest({
+      scope: "openid refresh",
+      redirectUri: config.client.redirect_uri,
+      connectionId,
+      userId,
+    })
+    console.log(data)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
