830b3c826474d1ed46257dd7646b202789219f55
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2020-09-03 12:48:43 +0100

    Update get auth-request script

--- examples/auth-requests/get-single.js
+++ examples/auth-requests/get-single.js
@@ -1,8 +1,24 @@
 const Moneyhub = require("../../src/index")
 const config = require("../config")
 
-// The id of the auth request
-const id = ""
+const commandLineArgs = require("command-line-args")
+const commandLineUsage = require("command-line-usage")
+
+const optionDefinitions = [
+  {name: "id", alias: "i", type: String, description: "required"},
+]
+
+const usage = commandLineUsage({
+  header: "Options",
+  optionList: optionDefinitions,
+})
+console.log(usage)
+
+const options = commandLineArgs(optionDefinitions)
+
+const {id} = options
+
+if (!id) throw new Error("Id is required")
 
 const start = async () => {
   try {
