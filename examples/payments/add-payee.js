a560c575bf1c834b74b2e08aa771f0e77dc2f5a3
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2020-09-10 16:16:47 +0100

    Update example script to accept external id

--- examples/payments/add-payee.js
+++ examples/payments/add-payee.js
@@ -7,6 +7,7 @@ const optionDefinitions = [
   {name: "account-number", alias: "a", type: String, description: "required"},
   {name: "sort-code", alias: "s", type: String, description: "required"},
   {name: "name", alias: "n", type: String, description: "required"},
+  {name: "external-id", alias: "i", type: String},
 ]
 
 const usage = commandLineUsage(
@@ -27,6 +28,7 @@ const start = async () => {
       accountNumber: options["account-number"],
       sortCode: options["sort-code"],
       name: options.name,
+      externalId: options["external-id"],
     })
 
     console.log(result)
