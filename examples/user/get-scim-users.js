94ded3eabf5e4c93ccda3f135ad2b103c1674df6
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2020-12-15 11:30:04 +0000

    Add example for scim users

---
+++ examples/user/get-scim-users.js
@@ -0,0 +1,32 @@
+const Moneyhub = require("../../src/index")
+const config = require("../config")
+
+const commandLineArgs = require("command-line-args")
+const commandLineUsage = require("command-line-usage")
+
+const optionDefinitions = [
+  {name: "limit", alias: "l", type: Number, defaultValue: 10},
+  {name: "offset", alias: "o", type: Number, defaultValue: 0},
+]
+
+const usage = commandLineUsage({
+  header: "Options",
+  optionList: optionDefinitions,
+})
+
+console.log(usage)
+
+const {limit, offset} = commandLineArgs(optionDefinitions)
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+
+    const result = await moneyhub.getSCIMUsers({limit, offset})
+    console.log(JSON.stringify(result, null, 2))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
