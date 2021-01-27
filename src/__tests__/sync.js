40b0355794cf8fbc276d1f43bd733c303c0e3070
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2021-01-27 12:03:35 +0000

    Add test for sync

---
+++ src/__tests__/sync.js
@@ -0,0 +1,23 @@
+/* eslint-disable max-nested-callbacks */
+const Moneyhub = require("..")
+const config = require("../../examples/config.local")
+const {expect} = require("chai")
+
+describe("Sync", () => {
+  let moneyhub
+  let connectionId
+  const userId = config.testUserIdWithconnection
+
+  before(async () => {
+    moneyhub = await Moneyhub(config)
+  })
+
+  it("sync user connection", async () => {
+    const user = await moneyhub.getUser({userId})
+    connectionId = user.connectionIds[0]
+    const result = await moneyhub.syncUserConnection({userId, connectionId})
+    expect(result.data.status).to.equal("ok")
+
+  })
+
+})
