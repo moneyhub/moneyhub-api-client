fce8fcfecf32cadb552cca84091bc42c5cd2bc97
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2021-02-17 10:09:26 +0000

    Update tests

--- src/__tests__/sync.js
+++ src/__tests__/sync.js
@@ -14,9 +14,18 @@ describe("Sync", () => {
 
   it("sync user connection", async () => {
     const user = await moneyhub.getUser({userId})
+    console.log(user)
     connectionId = user.connectionIds[0]
-    const result = await moneyhub.syncUserConnection({userId, connectionId})
-    expect(result.data.status).to.equal("ok")
+    try {
+      const result = await moneyhub.syncUserConnection({userId, connectionId})
+      expect(result.data.status).to.equal("ok")
+    } catch (error) {
+      // Even if a 500 is returned we are testing that the method calls the api
+      const {statusCode, body} = error.response
+      const responseBody = JSON.parse(body)
+      expect(statusCode).to.eql(500)
+      expect(responseBody).has.property("correlationId")
+    }
 
   })
 
