496f2b79f166be9c24264d00471ebea31e3d76b5
Gilbert Holland-Lloyd <gilbert.holland-lloyd@moneyhub.com>
2021-06-22 11:55:57 +0100

    Add test for auth-urls
    MH3-5862

--- src/__tests__/auth-urls.js
+++ src/__tests__/auth-urls.js
@@ -2,10 +2,18 @@
 const Moneyhub = require("..")
 const config = require("../../test/test-client-config")
 const {expect} = require("chai")
+const querystring = require("querystring")
 
 const bankId = "1ffe704d39629a929c8e293880fb449a"
 const state = "sample-state"
 const nonce = "sample-nonce"
+
+const parseJwt = (token) => {
+  const base64String = token.split(".")[1]
+  const decodedValue = JSON.parse(Buffer.from(base64String,
+    "base64").toString("ascii"))
+  return decodedValue
+}
 
 describe("Auth Urls", () => {
   let moneyhub
@@ -21,7 +29,26 @@ describe("Auth Urls", () => {
       scope: `openid id:${bankId} accounts:read`,
     })
 
+    const {request} = querystring.parse(url)
+    const payload = parseJwt(request)
+
     expect(url).to.be.a("string")
+    expect(payload).to.not.have.nested.property("claims.id_token.mh:consent.value.permissions")
+  })
+
+  it("gets a basic auth url with permissions", async () => {
+    const url = await moneyhub.getAuthorizeUrl({
+      state,
+      nonce,
+      scope: `openid id:${bankId} accounts:read`,
+      permissions: ["permission-1"]
+    })
+
+    const {request} = querystring.parse(url)
+    const payload = parseJwt(request)
+
+    expect(url).to.be.a("string")
+    expect(payload).to.have.deep.nested.property("claims.id_token.mh:consent.value.permissions", ["permission-1"])
   })
 
   it("gets an auth url for a user", async () => {
@@ -32,7 +59,27 @@ describe("Auth Urls", () => {
       userId: "some-user-id",
     })
 
+    const {request} = querystring.parse(url)
+    const payload = parseJwt(request)
+
     expect(url).to.be.a("string")
+    expect(payload).to.not.have.nested.property("claims.id_token.mh:consent.value.permissions")
+  })
+
+  it("gets an auth url for a user with extra permissions", async () => {
+    const url = await moneyhub.getAuthorizeUrlForCreatedUser({
+      state,
+      nonce,
+      bankId,
+      userId: "some-user-id",
+      permissions: ["permission-1"]
+    })
+
+    const {request} = querystring.parse(url)
+    const payload = parseJwt(request)
+
+    expect(url).to.be.a("string")
+    expect(payload).to.have.deep.nested.property("claims.id_token.mh:consent.value.permissions", ["permission-1"])
   })
 
   it("gets a payment auth url", async () => {
