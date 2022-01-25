0d11752399d1ae7363a4b480d94274e6cf79d257
Jamie Haywood <jamie.haywood@vodafone.com>
2022-01-25 21:49:41 +0000

    refactor: export default request array

---
+++ src/requests/index.ts
@@ -0,0 +1,35 @@
+import accounts from "./accounts";
+import authRequests from "./auth-requests";
+import beneficiaries from "./beneficiaries";
+import payees from "./payees";
+import payments from "./payments";
+import projects from "./projects";
+import sync from "./sync";
+import tax from "./tax";
+import transactions from "./transactions";
+import transactionFiles from "./transaction-files";
+import transactionSplits from "./transaction-splits";
+import unauthenticated from "./unauthenticated";
+import usersAndConnections from "./users-and-connections";
+import categories from "./categories";
+import standingOrders from "./standing-orders";
+import regularTransactions from "./regular-transactions";
+
+export default [
+  accounts,
+  authRequests,
+  beneficiaries,
+  payees,
+  payments,
+  projects,
+  sync,
+  tax,
+  transactions,
+  transactionFiles,
+  transactionSplits,
+  unauthenticated,
+  usersAndConnections,
+  categories,
+  standingOrders,
+  regularTransactions,
+];
