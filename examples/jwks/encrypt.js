c7575602037194cc1ab057a46c0928925db69b95
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2022-08-30 14:14:17 +0300

    Update encryption script

--- examples/jwks/encrypt.js
+++ examples/jwks/encrypt.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-statements */
-const {JWE, JWKS} = require("jose")
+const {importJWK, CompactEncrypt} = require("jose")
 const fs = require("fs")
 const got = require("got")
 const R = require("ramda")
@@ -10,23 +10,28 @@ const run = async () => {
     console.log("Please add the file you wish to encrypt as the final argument")
     process.exit(1)
   }
-  const fileContents = fs.readFileSync(file).toString()
+  const fileContents = fs.readFileSync(file)
   console.log(`Encrypting ${file}`)
 
   console.log("Retreiving Moneyhub encryption key")
-  const openidConfig = await got(`${config.identityServiceUrl}/.well-known/openid-configuration`).json()
+  const openidConfig = await got(
+    `${config.identityServiceUrl}/oidc/.well-known/openid-configuration`
+  ).json()
   const certs = await got(openidConfig.jwks_uri).json()
-  const keyStore = JWKS.asKeyStore(certs)
-  const key = keyStore.get({use: "enc"})
+
+  const jwk = certs.keys.find((key) => key.use === "enc")
+  const key = await importJWK(jwk, "ECDH-ES")
 
   if (!key) {
     console.log("No encryption key found")
     process.exit(1)
   }
 
-  console.log(`Encrypting with key id: ${key.kid}`)
+  console.log(`Encrypting with key id: ${jwk.kid}`)
 
-  const token = JWE.encrypt(fileContents, key)
+  const token = await new CompactEncrypt(fileContents)
+    .setProtectedHeader({alg: "ECDH-ES", enc: "A128CBC-HS256"})
+    .encrypt(key)
 
   console.log("Encryption complete")
   console.log("--------")
