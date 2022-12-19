8c47da8579b0283bb7a19c2d69b28cbad66a9fe5
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2022-12-19 17:21:12 +0200

    Remove .only

--- src/__tests__/transaction-splits.ts
+++ src/__tests__/transaction-splits.ts
@@ -11,7 +11,7 @@ const delay = (time: number) => {
   })
 }
 
-describe.only("Transaction Splits", function() {
+describe("Transaction Splits", function() {
   let moneyhub: MoneyhubInstance
   let transactionId: string
   let splitId: string
