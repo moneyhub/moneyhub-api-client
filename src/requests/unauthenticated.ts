f423a83ba3ca17f239d578539ffcb7ba7e28e088
Luke <44617984+lsparey@users.noreply.github.com>
2023-01-04 13:24:30 +0000

    Add clientId query to filter listConnections (#45)

--- src/requests/unauthenticated.ts
+++ src/requests/unauthenticated.ts
@@ -1,3 +1,4 @@
+import qs from "query-string"
 import {RequestsParams} from "../request"
 import {UnauthenticatedRequests} from "./types/unauthenticated"
 
@@ -5,20 +6,20 @@ export default ({config, request}: RequestsParams): UnauthenticatedRequests => {
   const {resourceServerUrl, identityServiceUrl} = config
   return {
     getGlobalCounterparties: (params = {}) =>
-      request(resourceServerUrl + "/global-counterparties", {
+      request(`${resourceServerUrl}/global-counterparties`, {
         searchParams: params,
       }),
-    listConnections: () =>
-      request(identityServiceUrl + "/oidc/.well-known/all-connections"),
-    listAPIConnections: () =>
-      request(identityServiceUrl + "/oidc/.well-known/api-connections"),
-    listTestConnections: () =>
-      request(identityServiceUrl + "/oidc/.well-known/test-connections"),
-    listPaymentsConnections: () =>
-      request(identityServiceUrl + "/oidc/.well-known/payments-connections"),
-    listBetaConnections: () =>
-      request(identityServiceUrl + "/oidc/.well-known/beta-connections"),
+    listConnections: (query?: {clientId?: string}) =>
+      request(`${identityServiceUrl}/oidc/.well-known/all-connections?${query && qs.stringify(query)}`),
+    listAPIConnections: (query?: {clientId?: string}) =>
+      request(`${identityServiceUrl}/oidc/.well-known/api-connections?${query && qs.stringify(query)}`),
+    listTestConnections: (query?: {clientId?: string}) =>
+      request(`${identityServiceUrl}/oidc/.well-known/test-connections?${query && qs.stringify(query)}`),
+    listPaymentsConnections: (query?: {clientId?: string}) =>
+      request(`${identityServiceUrl}/oidc/.well-known/payments-connections?${query && qs.stringify(query)}`),
+    listBetaConnections: (query?: {clientId?: string}) =>
+      request(`${identityServiceUrl}/oidc/.well-known/beta-connections?${query && qs.stringify(query)}`),
     getOpenIdConfig: () =>
-      request(identityServiceUrl + "/oidc/.well-known/openid-configuration"),
+      request(`${identityServiceUrl}/oidc/.well-known/openid-configuration`),
   }
 }
