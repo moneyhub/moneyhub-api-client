b6cf362a91b8438efbcdabba193aeaec67550852
Ingridaceg <ingrid.acero@moneyhub.com>
2024-01-09 12:08:20 -0500

    API-515 Add fields to POST auth-requests

--- examples/auth-requests/create.js
+++ examples/auth-requests/create.js
@@ -9,6 +9,8 @@ const optionDefinitions = [
   {name: "userId", alias: "u", type: String, description: "required"},
   {name: "bankId", alias: "b", defaultValue: DEFAULT_BANK_ID, type: String},
   {name: "permissions", alias: "p", description: "comma separated extra permissions", type: String},
+  {name: "ip", alias: "i", description: "ip-address", type: String},
+  {name: "customerLastLoggedTime", alias: "t", type: String},
 ]
 
 const usage = commandLineUsage({
@@ -19,7 +21,7 @@ console.log(usage)
 
 const options = commandLineArgs(optionDefinitions)
 
-const {userId, bankId, permissions} = options
+const {userId, bankId, permissions, ip, customerLastLoggedTime} = options
 
 if (!userId) throw new Error("userId is required")
 
@@ -30,7 +32,9 @@ const start = async () => {
       scope: `openid id:${bankId}`,
       redirectUri: config.client.redirect_uri,
       userId,
-      permissions: permissions && permissions.split(",")
+      permissions: permissions && permissions.split(","),
+      customerIpAddress: ip,
+      customerLastLoggedTime
     })
     console.log(data)
   } catch (e) {
