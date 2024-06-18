c5d5b6c62b47fa93dea38d2d4bf8b5f40356f552
Jake Fenley <145364296+jakefenley-mh@users.noreply.github.com>
2024-06-18 15:01:05 +0100

    Update pay-link types with new params (#86)

--- src/schema/pay-link.ts
+++ src/schema/pay-link.ts
@@ -3,17 +3,21 @@ import {SearchParams} from "../request"
 export interface PayLinkSearchParams extends SearchParams {
   widgetId?: string
   payeeId?: string
+  isActive?: boolean
 }
 
 export interface PayLink {
   id: string
   clientId: string
+  widgetId: string
   payeeId: string
   reference: string
-  widgetId: string
-  amount?: number
-  expiry?: string
-  endToEndId?: string
+  amount: number
+  useOnce: boolean
   createdAt: string
   updatedAt: string
+  archivedAt: string | null
+  usedAt: string | null
+  endToEndId: string | null
+  expiresAt: string | null
 }
