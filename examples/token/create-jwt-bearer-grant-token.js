1ffc4a6c4bb729fbf6b68affce905b69609ccc1e
ANNA DUHOVNY <anna.duhovny@moneyhub.com>
2025-01-08 14:17:36 +0000

    API-1217 Sub fix bug

--- examples/token/create-jwt-bearer-grant-token.js
+++ examples/token/create-jwt-bearer-grant-token.js
@@ -4,29 +4,25 @@ const commandLineArgs = require("command-line-args")
 const commandLineUsage = require("command-line-usage")
 
 const optionDefinitions = [
-  {name: "userId", alias: "u", type: String, description: "required"},
+  {name: "subject", alias: "s", type: String, description: "required (can be userId or clientId)"},
 ]
 
-const usage = commandLineUsage(
-  {
-    header: "Options",
-    optionList: optionDefinitions,
-  }
-)
+const usage = commandLineUsage({
+  header: "Options",
+  optionList: optionDefinitions,
+})
 
 console.log(usage)
 
 const options = commandLineArgs(optionDefinitions)
 
-if (!options.userId) throw new Error("userId needs to be provided")
+if (!options.subject) throw new Error("subject (userId or clientId) needs to be provided")
 
 const start = async () => {
   try {
     const moneyhub = await Moneyhub(config)
 
-    const data = await moneyhub.createJWTBearerGrantToken({
-      sub: options.userId,
-    })
+    const data = await moneyhub.createJWTBearerGrantToken(options.subject)
     console.log(JSON.stringify(data, null, 2))
   } catch (e) {
     console.log(e)
