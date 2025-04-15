55f5c7a0163d63a094281b77069181b08cba699d
Jake Fenley <jake.fenley@moneyhub.com>
2025-04-15 11:52:38 +0100

    Create example reseller check request

---
+++ examples/reseller-check/create-reseller-check.js
@@ -0,0 +1,37 @@
+const commandLineArgs = require("command-line-args")
+const commandLineUsage = require("command-line-usage")
+const {Moneyhub} = require("../../src/index")
+const config = require("../config")
+
+const optionDefinitions = [
+  {name: "companyRegistrationNumber", alias: "c", type: String, description: "required"},
+  {name: "email", alias: "e", type: String, description: "required"},
+  {name: "telephone", alias: "t", type: String},
+]
+
+const usage = commandLineUsage(
+  {
+    header: "Options",
+    optionList: optionDefinitions,
+  }
+)
+console.log(usage)
+
+const options = commandLineArgs(optionDefinitions)
+
+const start = async () => {
+  try {
+    const moneyhub = await Moneyhub(config)
+
+    const result = await moneyhub.createResellerCheckRequest({
+      companyRegistrationNumber: options.companyRegistrationNumber,
+      email: options.email,
+      telephone: options.telephone,
+    })
+    console.log(JSON.stringify(result, null, 2))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
