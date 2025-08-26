a2d26946dcfb8cc76bc5a91e764cf10f80d67b75
Jake Fenley <jake.fenley@moneyhub.com>
2025-08-26 10:44:39 +0100

    Add example scripts

--- src/schema/transaction.ts
+++ src/schema/transaction.ts
@@ -69,6 +69,7 @@ export type TransactionUnenrichedSearchParams = Pick<
   | "endDate"
   | "startDateModified"
   | "endDateModified"
+  | "text"
   | "accountId"
   | "accountIds"
   | "creditDebitIndicator"
